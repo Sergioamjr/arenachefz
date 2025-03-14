3c0a794417a435dfddae37512797b3bf062b0a24
Sérgio Júnior <sergioamjr91@gmail.com>
2025-03-14 19:31:26 -0300

    fixing api url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Home from './../components/Home';
 
 export default async function Page() {
-  const data = await fetch('http://localhost:3000/api');
+  const prop = 'https://arenachefz.vercel.app/api';
+  const local = 'http://localhost:3000/api';
+  const url = process.env.NODE_ENV === 'production' ? prop : local;
+  const data = await fetch(url);
   const toJson = await data.json();
 
-  console.log('>>>', toJson);
   return <Home {...toJson} />;
 }
